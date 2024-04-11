d55aadec837134ab79e0f434c9b6dec7c6e096a6
Nayram Mensah <nayrammensah@gmail.com>
2024-04-11 12:22:24 +0200

    feat: add activity migration script

--- docker/init-db.sql
+++ docker/init-db.sql
@@ -1,2 +1,7 @@
 CREATE DATABASE "koa_health_user_activity_manager";
+\c koa_health_user_activity_manager
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE DATABASE "koa_health_user_activity_manager-test";
+\c koa_health_user_activity_manager_test
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
---
+++ src/scripts/migrations/2024.04.10T22.25.25.create-activity-table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE "activity" (
+    "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
+    "title" character varying NOT NULL,
+    "description" TEXT,
+    "category" character varying,
+    "duration" BIGINT,
+    "difficulty_level" character varying,
+    "content" TEXT,
+    "created_at" TIMESTAMP NOT NULL DEFAULT now(),
+    "updated_at" TIMESTAMP DEFAULT now(),
+    CONSTRAINT "PK_8d12ff38fcc62cd7b25642b2d19" PRIMARY KEY ("id")
+);
