191cc318cb3a9a8ff48e41bf3400b3f1d0d66102
Nayram Mensah <nayrammensah@gmail.com>
2024-04-11 20:06:45 +0200

    feat(model): add function to create activities

--- src/ model/activities/index.ts
+++ src/ model/activities/index.ts
@@ -1,17 +1,38 @@
 import { sql } from "@pgkit/client";
 import { dbClient } from "../../lib/postgres-utils/resource";
 
-type Activity = {
-  id: string;
+export enum Category {
+  RELAXATION = 'RELAXATION',
+  PHYSICAL_HEALTH = 'PHYSICAL_HEALTH',
+  PRODUCTIVITY = 'PRODUCTIVITY',
+  SELF_ESTEEM = 'SELF_ESTEEM',
+  SOCIAL_CONNECTION = 'SOCIAL_CONNECTION',
+}
+
+export enum DifficultyLevel { 
+  BEGINNER = 'BEGINNER',
+  INTERMEDIATE = 'INTERMEDIATE',
+  ADVANCED = 'ADVANCED',
+  EXPERT = 'EXPERT',
+}
+
+export type Activity = {
   title: string;
   description: string;
-  category: string;
+  category: Category;
   duration: number;
-  difficulty_level: string;
+  difficulty_level: DifficultyLevel;
   content: string;
-  created_at: Date;
-  updated_at: Date;
+}
+
+export type ActivitySchema = Activity & { id: string, created_at: Date, updated_at: Date };
+
+export const getAllActivities = async (): Promise<ActivitySchema[]> => {
+  return await dbClient.many(sql<ActivitySchema>`SELECT * FROM activity;`);
 };
-export const getAllActivities = async (): Promise<Activity[]> => {
-  return await dbClient.many(sql<Activity>`SELECT * FROM activity;`);
-};
+
+export const createActivities = async (activities: Activity[]) => {
+  const queryValues = activities.map(activity => `('${activity.title}', '${activity.description}', '${activity.category}', '${activity.duration}', '${activity.difficulty_level}', '${activity.content}')`).join(', ');
+  let query = sql`INSERT INTO activity (title, description, category, duration, difficulty_level, content) VALUES ${sql.raw(queryValues)};`;
+  await dbClient.query(query);
+}
---
+++ src/ model/activities/tests/create-activities.spec.ts
@@ -0,0 +1,53 @@
+import { sql } from '@pgkit/client';
+import { dbClient } from '../../../lib/postgres-utils/resource';
+import { createActivities, Category, DifficultyLevel, ActivitySchema } from '../.';
+
+describe('Models: Create activities', () => { 
+
+    afterAll(async () => {
+        await dbClient.query(sql`DELETE FROM activity;`);
+        await dbClient.end();
+    })
+
+    test("should create activities", async () => {
+        const activities = [
+            {
+                title: 'Mindful Breathing',
+                description: 'A simple activity to bring your focus to your breath and reduce stress.',
+                category: Category.RELAXATION,
+                duration: 300,
+                difficulty_level: DifficultyLevel.BEGINNER,
+                content: 'Inhale slowly through your nose, hold for a few seconds, then exhale slowly.'
+            },
+            {
+                title: 'Yoga for Beginners',
+                description: 'Start your yoga journey with some basic poses aimed at improving flexibility and relaxation.',
+                category: Category.PHYSICAL_HEALTH,
+                duration: 1800,
+                difficulty_level: DifficultyLevel.BEGINNER,
+                content: 'Follow a series of beginner-friendly yoga poses, focusing on your breath and alignment.'
+            },
+            {
+                title: 'Pomodoro Technique',
+                description: 'Improve productivity by breaking your work into intervals, traditionally 25 minutes in length, separated by short breaks.',
+                category: Category.PRODUCTIVITY,
+                duration: 1500,
+                difficulty_level: DifficultyLevel.BEGINNER,
+                content: 'Work for 25 minutes, then take a 5-minute break. Repeat the cycle.'
+            },
+        ];
+        await createActivities(activities);
+        const results = await dbClient.many(sql<ActivitySchema>`SELECT * FROM activity;`)
+        expect(results).toHaveLength(activities.length);
+        results.forEach(result => {
+            expect(activities).toContainEqual({
+                title: result.title,
+                description: result.description,
+                category: result.category,
+                duration: result.duration,
+                difficulty_level: result.difficulty_level,
+                content: result.content
+            })
+        })
+    })
+ })
