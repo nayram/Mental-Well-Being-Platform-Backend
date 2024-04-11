1d361c14905efe3387fe1031489fbfcd0c00da66
Nayram Mensah <nayrammensah@gmail.com>
2024-04-11 17:58:18 +0200

    feat: setup api server and run on a port

---
+++ src/api/app.ts
@@ -0,0 +1,25 @@
+import express, { Application } from 'express';
+import pino from 'pino-http';
+import helmet from 'helmet';
+import cors from 'cors';
+import { logger } from '../lib/logger';
+import { appRoutes } from './routes'
+
+const app = express();
+
+const log = logger({ serviceName: 'api' })
+
+const requestLogger = pino({
+  logger: log
+})
+
+export const createApp = (): Application => {
+    app
+    .use(requestLogger)
+    .use(cors())
+    .use(helmet())
+      .use(express.urlencoded({ extended: true }))
+      .use(express.json())
+      .use(appRoutes)
+  return app
+}
---
+++ src/api/server.ts
@@ -0,0 +1,54 @@
+import config from 'config'
+import { Response } from 'express'
+import { Server } from 'http'
+import { createApp } from './app'
+import { logger } from 'lib/logger'
+import { dbClient } from '../lib/postgres-utils/resource'
+
+let server: Server
+
+const log = logger({
+    serviceName: 'api'
+})
+
+const delay = 3 * 2 * 1000 // 3 seconds
+
+const bootstrap = async () => {
+    const port = config.get<number>('port')
+    const app = createApp()
+    app.get('/', (_, res: Response) => {
+        res.send(`Listening on port: ${port}`);
+    })
+    server = app.listen(port, () => {
+        log.info(`Server running successfully on port ${port}`)
+    })
+}
+
+const shutDownServices = async () => {
+    log.info('Closing database connection...')
+    await dbClient.end();
+    log.info("Database connection closed");
+    log.info("Server shutting down...")
+    server.close(() => log.info("Server closed"));
+}
+
+process
+.on("SIGTERM", (err) => {
+  if (err) log.error(err)
+  
+  return setTimeout( async () => {
+   await shutDownServices()
+  }, delay)
+})
+.on("SIGINT", (err) => {
+  if (err) console.error(err)
+  log.info("Server shutting down...")
+  return setTimeout(async () => {
+    await shutDownServices()
+  }, delay)
+})
+.on("unhandledRejection", (error) => {
+  log.error(error)
+})
+
+bootstrap()
