bc66df1fe11616c404cf7b7e350b06c1f6945d99
Nayram Mensah <nayrammensah@gmail.com>
2024-04-13 17:02:45 +0200

    feat(model): rename and update function to get user by email

--- src/models/users/tests/get-user-by-email.spec.ts
+++ src/models/users/tests/get-user-by-email.spec.ts
@@ -1,6 +1,6 @@
 import { sql } from "@pgkit/client";
 import { dbClient } from "../../../lib/postgres-utils/resource";
-import { getUserByEmailAndPassword, UserTableName } from "..";
+import { getUserByEmail, UserTableName } from "..";
 import { truncateTable, closeDb } from "../../../helpers/utils";
 
 describe("Models: Get user by username", () => {
@@ -16,14 +16,15 @@ describe("Models: Get user by username", () => {
     const user = {
       username: "nayram_test",
       email: "nayrammensah@gmail.com",
-      password: "hashed_password",
+      password: "password",
     };
     await dbClient.query(
       sql`INSERT INTO ${sql.identifier(["user"])} (username, email, password) VALUES (${user.username}, ${user.email}, ${user.password});`,
     );
-    const getUser = await getUserByEmailAndPassword(user.email, user.password);
+    const getUser = await getUserByEmail(user.email);
     expect(getUser?.username).toBe(user.username);
     expect(getUser?.email).toBe(user.email);
+    expect(getUser?.password).toBe(user.password);
   });
 
   test("should return null if user does not exist", async () => {
@@ -32,7 +33,7 @@ describe("Models: Get user by username", () => {
       email: "nayrammensah@gmail.com",
       password: "hashed_password",
     };
-    const getUser = await getUserByEmailAndPassword(user.email, user.password);
+    const getUser = await getUserByEmail(user.email);
     expect(getUser).toBeNull();
   });
 });
