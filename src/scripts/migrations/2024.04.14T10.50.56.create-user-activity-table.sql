46ed2e7c4b3a2491007c6818fcf06a359f050ba5
Nayram Mensah <nayrammensah@gmail.com>
2024-04-14 20:44:08 +0200

    feat: update migration script ### activity - add field status to indicate whether an activity is active or not. - -- Active status means it's part of the activites to be performed. ---- deleted means the activity  is no more available to users ### user-activity ---- add foreign key constraints to user-activity table.

--- src/scripts/migrations/2024.04.14T10.50.56.create-user-activity-table.sql
+++ src/scripts/migrations/2024.04.14T10.50.56.create-user-activity-table.sql
@@ -6,6 +6,8 @@ CREATE TABLE "user-activty" (
     "status" character varying NOT NULL, 
     "created_at" TIMESTAMP NOT NULL DEFAULT now(), 
     "updated_at" TIMESTAMP DEFAULT now(), 
-    CONSTRAINT "UQ_user_activity_user_activity_id_user_id" UNIQUE ("user_id", "activity_id"),
+    CONSTRAINT "UQ_user_activity_id_user_id" UNIQUE ("user_id", "activity_id"),
+    CONSTRAINT "FK_user_id" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION,
+    CONSTRAINT "FK_activity_id" FOREIGN KEY ("activity_id") REFERENCES "activity"("id") ON DELETE NO ACTION ON UPDATE NO ACTION,
     CONSTRAINT "PK_yade7a159ff9t2512dd42343860" PRIMARY KEY ("id")
 )
